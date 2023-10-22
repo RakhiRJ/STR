79ef229065f68a0d0fa839bfb3be344750c071a4
RakhiRJ <144945767+RakhiRJ@users.noreply.github.com>
2023-10-22 08:54:02 +0530

    Add files via upload

---
+++ D.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class D {
+	public static void main(String[] args) {
+		
+		int a[][]= {  {1,2} ,{3,4} ,{5,6}  }; 
+		
+		System.out.println("no of row :" + a.length) ;
+		System.out.println("no of colum :"+a[0].length);
+		
+	}
+	
+}
